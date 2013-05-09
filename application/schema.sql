3d945f4cf0acdffa1c6b3e9ef71f4330c643fe80
Oreolek <keloero@oreolek.ru>
2013-05-09 10:46:06 +0700

    Kohana 3.1 has changed its Auth schema :-(

--- application/schema.sql
+++ application/schema.sql
@@ -42,61 +42,57 @@ CREATE TABLE `pages` (
 
 /* User management */
 
-DROP TABLE IF EXISTS `roles`;
-
-CREATE TABLE `roles` (
-  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(32) NOT NULL,
-  `description` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uniq_name` (`name`)
-) ENGINE=INNODB  DEFAULT CHARSET=utf8 ;
- 
-INSERT INTO `roles` (`id`, `name`, `description`) VALUES
-(1, 'login', 'Login privileges, granted after account confirmation'),
-(2, 'admin', 'Administrative user, has access to everything.');
- 
 DROP TABLE IF EXISTS `roles_users`;
-
-CREATE TABLE `roles_users` (
-  `user_id` INT(10) UNSIGNED NOT NULL,
-  `role_id` INT(10) UNSIGNED NOT NULL,
-  PRIMARY KEY (`user_id`,`role_id`),
-  KEY `fk_role_id` (`role_id`)
-) ENGINE=INNODB DEFAULT CHARSET=utf8;
- 
+DROP TABLE IF EXISTS `roles`;
+DROP TABLE IF EXISTS `user_tokens`;
 DROP TABLE IF EXISTS `users`;
 
+CREATE TABLE `roles` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `uniq_name` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+INSERT INTO `roles` (`id`, `name`, `description`) VALUES(1, 'login', 'Login privileges, granted after account confirmation');
+INSERT INTO `roles` (`id`, `name`, `description`) VALUES(2, 'admin', 'Administrative user, has access to everything.');
+
+CREATE TABLE `roles_users` (
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `role_id` int(10) UNSIGNED NOT NULL,
+  PRIMARY KEY  (`user_id`,`role_id`),
+  KEY `fk_role_id` (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `users` (
-  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `email` VARCHAR(127) NOT NULL,
-  `username` VARCHAR(32) NOT NULL DEFAULT '',
-  `password` CHAR(50) NOT NULL,
-  `logins` INT(10) UNSIGNED NOT NULL DEFAULT '0',
-  `last_login` INT(10) UNSIGNED DEFAULT NULL,
-  PRIMARY KEY (`id`),
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `email` varchar(254) NOT NULL,
+  `username` varchar(32) NOT NULL DEFAULT '',
+  `password` varchar(64) NOT NULL,
+  `logins` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `last_login` int(10) UNSIGNED,
+  PRIMARY KEY  (`id`),
   UNIQUE KEY `uniq_username` (`username`),
   UNIQUE KEY `uniq_email` (`email`)
-) ENGINE=INNODB  DEFAULT CHARSET=utf8 ;
- 
-DROP TABLE IF EXISTS `user_tokens`;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE `user_tokens` (
-  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `user_id` INT(11) UNSIGNED NOT NULL,
-  `user_agent` VARCHAR(40) NOT NULL,
-  `token` VARCHAR(32) NOT NULL,
-  `created` INT(10) UNSIGNED NOT NULL,
-  `expires` INT(10) UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`),
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) UNSIGNED NOT NULL,
+  `user_agent` varchar(40) NOT NULL,
+  `token` varchar(40) NOT NULL,
+  `created` int(10) UNSIGNED NOT NULL,
+  `expires` int(10) UNSIGNED NOT NULL,
+  PRIMARY KEY  (`id`),
   UNIQUE KEY `uniq_token` (`token`),
-  KEY `fk_user_id` (`user_id`)
-) ENGINE=INNODB DEFAULT CHARSET=utf8 ;
- 
- 
+  KEY `fk_user_id` (`user_id`),
+  KEY `expires` (`expires`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
 ALTER TABLE `roles_users`
   ADD CONSTRAINT `roles_users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `roles_users_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
- 
+
 ALTER TABLE `user_tokens`
   ADD CONSTRAINT `user_tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
