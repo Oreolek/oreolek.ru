a68061be438e8d70ed7d8a6769ebc23192f8873c
Oreolek <keloero@oreolek.ru>
2013-12-18 14:42:48 +0700

    Custom error pages

---
+++ application/classes/HTTP/Exception.php
@@ -0,0 +1,38 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+ /**
+  * Custom error pages fix
+  **/
+class HTTP_Exception extends Kohana_HTTP_Exception {
+    /**
+     * Generate a Response for all Exceptions without a more specific override
+     * 
+     * The user should see a nice error page, however, if we are in development
+     * mode we should show the normal Kohana error page.
+     * 
+     * @return Response
+     */
+    public function get_response()
+    {
+        // Lets log the Exception, Just in case it's important!
+        Kohana_Exception::log($this);
+ 
+        if (Kohana::$environment >= Kohana::DEVELOPMENT)
+        {
+            // Show the normal Kohana error page.
+            return parent::get_response();
+        }
+        else
+        {
+          // Generate a nicer looking "Oops" page using error sub request 
+          $attributes = array( 
+            'action' => $this->getCode(),
+            'message' => rawurlencode($this->getMessage())
+          ); 
+
+          return Request::factory(Route::get('error')->uri($attributes)) 
+            ->execute() 
+            ->send_headers() 
+            ->body();
+        }
+    }
+}
