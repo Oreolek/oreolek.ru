a670f697a84cd9bf402e44abeddb552c21381ddc
Oreolek <keloero@oreolek.ru>
2015-09-02 13:50:39 +0700

    gratipay already shows btc address

--- application/classes/View/Post/View.php
+++ application/classes/View/Post/View.php
@@ -81,7 +81,7 @@ class View_Post_View extends View_Layout {
   {
     return array(
       'heading' => 'Понравилось? Поддержите, чтобы таких статей было больше!',
-      'code' => '<iframe frameborder="0" allowtransparency="true" scrolling="no" src="https://money.yandex.ru/embed/small.xml?account=410011560528046&quickpay=small&any-card-payment-type=on&button-text=04&button-size=s&button-color=orange&targets=oreolek.ru+%D0%BD%D0%B0+%D0%BD%D0%BE%D0%B2%D1%8B%D0%B5+%D1%81%D1%82%D0%B0%D1%82%D1%8C%D0%B8+%D0%BF%D1%80%D0%BE...&default-sum=100&successURL=" width="158" height="31"></iframe><script data-gratipay-username="Oreolek" data-gratipay-widget="button" src="https://gttp.co/v1.js"></script><script src="/uploads/2014/09/coin.min.js"></script><script>CoinWidgetCom.go({wallet_address: "1GR5BqTJicETf9H6QnM1VsYwRw2cFcWiCb", currency: "bitcoin", counter: "amount", alignment: "bl", qrcode: false, auto_show: false, lbl_button: "Пожертвовать", lbl_address: "Мой адрес Bitcoin", lbl_count: "пожертвований", lbl_amount: "BTC"});</script>',
+      'code' => '<iframe frameborder="0" allowtransparency="true" scrolling="no" src="https://money.yandex.ru/embed/small.xml?account=410011560528046&quickpay=small&any-card-payment-type=on&button-text=04&button-size=s&button-color=orange&targets=oreolek.ru+%D0%BD%D0%B0+%D0%BD%D0%BE%D0%B2%D1%8B%D0%B5+%D1%81%D1%82%D0%B0%D1%82%D1%8C%D0%B8+%D0%BF%D1%80%D0%BE...&default-sum=100&successURL=" width="158" height="31"></iframe><script data-gratipay-username="Oreolek" data-gratipay-widget="button" src="https://gttp.co/v1.js"></script>',
     );
   }
 }
